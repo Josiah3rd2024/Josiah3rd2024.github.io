2f35ddb5b1e193e1755e9bdc4fcc0bbdff4aa14e
Josiah Rebolledo <rebojos0274@s.lfcisd.net>
2024-05-23 11:11:04 -0500

    Initial commit of my web page

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+    alert('Welcome to my web page!');
+   };
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = 'lightblue'; // Changes the background color to light blue
+   }
+   
+function toggleText() {
+    var paragraph = document.getElementById('toggleParagraph');
+    if (paragraph.style.display === 'none') {
+    paragraph.style.display = 'block';
+    } else {
+    paragraph.style.display = 'none';
+    }
+    }
+    
+$(document).ready(function() {
+     $("#tabs").tabs();
+    });
